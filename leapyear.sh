6d33ce1cae847801a776a9751e828821837ac0d0
Shubham <shubhamaryan805@gmail.com>
2022-06-22 22:00:31 +0530

    leap year prog

---
+++ leapyear.sh
@@ -0,0 +1,10 @@
+#! /bin/bash/ -x
+
+read -p"Enter a Year: " year 
+
+if (( $year % 400 == 0 || $year % 100 == 0 && $year % 100 != 0 ))
+then
+    echo "Leap Year"
+else
+    echo "Not a Leap Year"
+fi
