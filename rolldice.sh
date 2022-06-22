21eaec1699f3b0f0125fb6d894efa1bfa03f86ca
Shubham <shubhamaryan805@gmail.com>
2022-06-22 22:06:57 +0530

    rolling a dice prog

---
+++ rolldice.sh
@@ -0,0 +1,35 @@
+# Program to Role Dice using Switch case
+#! /bin/bash -x
+dice_num=$(( RANDOM%7  ))
+
+echo "Random number: " $dice_num
+
+case $dice_num in
+
+1) echo "One"
+
+;;
+
+2) echo "Two"
+
+;;
+
+3) echo "Three"
+
+;;
+
+4) echo "Four"
+
+;;
+
+5) echo "Five"
+
+;;
+
+6) echo "Six"
+
+;;
+
+*)  echo "Role again!"
+
+esac
